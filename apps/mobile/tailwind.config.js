8a8aee17549492ba33b5e36eb9c53e6c4a374cf7
Benedikt Weyer <bw.development@pm.me>
2026-05-31 22:33:29 +0200

    feat: add nativewind preset to Tailwind CSS configuration

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{ts,tsx}', './index.ts', './src/**/*.{ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {
       colors: {
